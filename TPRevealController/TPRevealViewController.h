a322bdd24e9c6b99fe4815eeb2bad512a581f199
chuibb001 <yansimon@simonyuyan-pc2.local>
2013-11-25 11:59:25 +0800

    update

--- TPRevealController/TPRevealViewController.h
+++ TPRevealController/TPRevealViewController.h
@@ -11,47 +11,45 @@
 
 typedef enum
 {
-    TPRevealSwipeDirectionLeft,
-    TPRevealSwipeDirectionRight
-}
-TPRevealSwipeDirection;
-
-typedef enum
-{
     TPRevealViewControllerLeft,
     TPRevealViewControllerRight,
+    
     TPRevealViewControllerCenter,
 }
 TPRevealViewControllerType;
 
-@interface TPRevealViewController : UIViewController
+@interface TPRevealViewController : UIViewController<UIGestureRecognizerDelegate>
 
 @property (nonatomic,strong) UIViewController *leftViewController;
 @property (nonatomic,strong) UIViewController *rightViewController;
 @property (nonatomic,strong) UIViewController *rootViewController;
+@property (nonatomic,assign) CGFloat leftOffSet;
+@property (nonatomic,assign) CGFloat rightOffSet;
 
-@property (nonatomic,strong) UIPanGestureRecognizer *pan;
-@property (nonatomic,strong) UITapGestureRecognizer *tap;
-@property (nonatomic,assign) CGFloat offSet;             // 偏移量
-@property (nonatomic,assign) CGFloat minX;
-@property (nonatomic,assign) CGFloat maxX;
-@property (nonatomic,assign) CGRect baseRect;            // root view当前位置的Rect
-@property (nonatomic,assign) const CGRect leftRect;      // root view最左位置的Rect
-@property (nonatomic,assign) const CGRect centerRect;    // root view中间位置的Rect
-@property (nonatomic,assign) const CGRect rightRect;     // root view最右位置的Rect
-@property (nonatomic,assign) CGPoint velocity;           // 滑动速率
-@property (nonatomic,assign) TPRevealSwipeDirection direction;  // 滑动方向
-@property (nonatomic,assign) TPRevealViewControllerType upperController;   // 在上面的视图
-@property (nonatomic,assign) BOOL canSwipe;
 
--(void)showRootViewController;
++(id)sharedInstance;
 
--(void)showLeftViewController;
+/**
+ *  show viewcontroller
+ */
+-(void)showRootViewControllerAnimated:(BOOL)animate;
+-(void)showLeftViewControllerAnimated:(BOOL)animate;
+-(void)showRightViewControllerAnimated:(BOOL)animate;
 
--(void)showRightViewController;
-
+/**
+ *  enable viewcontroller
+ */
 -(void)setLeftViewControllerEnable:(BOOL)enable;
-
 -(void)setRightViewControllerEnable:(BOOL)enable;
+
+/**
+ *  change root viewcontroller
+ */
+-(void)changeRootViewController:(UIViewController *) rootController;
+
+/**
+ *  is showing root viewcontroller
+ */
+-(BOOL)isCentered;
 
 @end
--- TPRevealController/TPRevealViewController.m
+++ TPRevealController/TPRevealViewController.m
@@ -8,19 +8,38 @@
 
 #import "TPRevealViewController.h"
 
+typedef enum
+{
+    TPRevealSwipeDirectionLeft,
+    TPRevealSwipeDirectionRight
+}
+TPRevealSwipeDirection;
+
 @interface TPRevealViewController ()
 
+@property (nonatomic,assign) CGRect baseRect; // current frame of rootViewcontroller
+@property (nonatomic,assign) CGFloat leftBorder;
+@property (nonatomic,assign) CGFloat rightBorder;
+@property (nonatomic,strong) UIPanGestureRecognizer *pan;
+@property (nonatomic,strong) UITapGestureRecognizer *tap;
+@property (nonatomic,assign) const CGRect leftRect; // left frame of rootViewcontroller
+@property (nonatomic,assign) const CGRect centerRect; // center frame of rootViewcontroller
+@property (nonatomic,assign) const CGRect rightRect; // right frame of rootViewcontroller
+@property (nonatomic,assign) CGPoint velocity; // swipe speed
+@property (nonatomic,assign) TPRevealSwipeDirection direction; // swipe direction
+@property (nonatomic,assign) TPRevealViewControllerType upperController;
+@property (nonatomic,assign) BOOL canSwipe;
 @end
 
 @implementation TPRevealViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+static TPRevealViewController *instance;
+
++ (id)sharedInstance
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+    if(!instance)
+        instance = [[TPRevealViewController alloc] init];
+    return instance;
 }
 
 - (void)viewDidLoad
@@ -32,163 +51,226 @@
     
 }
 
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
 #pragma mark Gesture Recognizer
 - (void)gestureRecognizerDidPan:(UIPanGestureRecognizer*)panGesture
 {
-    // 记录速率
-    self.velocity = [self.pan velocityInView:self.view];
-    // 相对坐标
-    CGPoint point = [panGesture translationInView:self.view];
-    // 判断方向
-    if(point.x > 0)
-        self.direction = TPRevealSwipeDirectionRight;
-    else
-        self.direction = TPRevealSwipeDirectionLeft;
-
-    // Root View 产生位移的条件:在边界范围内,产生>3的位移,标志允许
-    if(self.baseRect.origin.x + point.x < self.maxX && self.baseRect.origin.x + point.x > self.minX && fabsf(point.x) > 3.0 && self.canSwipe)
+    /**
+     *  what to do in the finger down state is to change root viewcontroller's frame
+     */
+    if(panGesture.state == UIGestureRecognizerStateChanged)
     {
-        self.rootViewController.view.frame = CGRectMake(self.baseRect.origin.x + point.x, self.rootViewController.view.frame.origin.y, self.rootViewController.view.frame.size.width, self.rootViewController.view.frame.size.height);
-        // 调整Left/Right View的顺序,避免遮挡
-        if(self.leftViewController && self.rightViewController)
+        // record swipe speed
+        self.velocity = [self.pan velocityInView:self.view];
+        // relative coordinate
+        CGPoint point = [panGesture translationInView:self.view];
+        // record swipe direction
+        if(point.x > 0)
+            self.direction = TPRevealSwipeDirectionRight;
+        else
+            self.direction = TPRevealSwipeDirectionLeft;
+        
+        
+        if(self.baseRect.origin.x + point.x < self.rightBorder && self.baseRect.origin.x + point.x > self.leftBorder && fabsf(point.x) > 3.0 && self.canSwipe)
         {
-            if (self.rootViewController.view.frame.origin.x > 0 && self.upperController != TPRevealSwipeDirectionLeft) {
-                [self.leftViewController.view removeFromSuperview];
-                [self.view insertSubview:self.leftViewController.view aboveSubview:self.rightViewController.view];
-                self.upperController = TPRevealSwipeDirectionLeft;
-            }
-            else if (self.rootViewController.view.frame.origin.x < 0 && self.upperController != TPRevealSwipeDirectionRight)
+            self.rootViewController.view.frame = CGRectMake(self.baseRect.origin.x + point.x, self.rootViewController.view.frame.origin.y, self.rootViewController.view.frame.size.width, self.rootViewController.view.frame.size.height);
+            // ajust the order between left and right viewcontroller
+            if(self.leftViewController && self.rightViewController)
             {
-                [self.rightViewController.view removeFromSuperview];
-                [self.view insertSubview:self.rightViewController.view aboveSubview:self.leftViewController.view];
-                self.upperController = TPRevealSwipeDirectionRight;
+                if (self.rootViewController.view.frame.origin.x > 0 && self.upperController != TPRevealSwipeDirectionLeft) {
+                    [self.view exchangeSubviewAtIndex:0 withSubviewAtIndex:1];
+                    self.upperController = TPRevealSwipeDirectionLeft;
+                }
+                else if (self.rootViewController.view.frame.origin.x < 0 && self.upperController != TPRevealSwipeDirectionRight)
+                {
+                    [self.view exchangeSubviewAtIndex:0 withSubviewAtIndex:1];
+                    self.upperController = TPRevealSwipeDirectionRight;
+                }
             }
         }
     }
+    /**
+     *  When finger up , determine which position is the end
+     */
+    else if(panGesture.state == UIGestureRecognizerStateEnded)
+    {
+        CGFloat minX = CGRectGetMinX(self.rootViewController.view.frame);
+        CGFloat maxX = CGRectGetMaxX(self.rootViewController.view.frame);
+        CGRect endRect = CGRectZero;
+        
+        BOOL shouldQuickSwipe = NO;
+        if(self.velocity.x > 200.0 || self.velocity.x < -200.0) // quick swipe
+        {
+            shouldQuickSwipe = YES;
+            
+        }
+        
+        if (minX > 0) {
+            if (!shouldQuickSwipe) {
+                if (minX < self.leftOffSet / 2) {
+                    endRect = self.centerRect;
+                } else {
+                    endRect = self.rightRect;
+                }
+            } else {
+                switch (self.direction) {
+                    case TPRevealSwipeDirectionLeft:
+                        endRect = self.centerRect;
+                        break;
+                    case TPRevealSwipeDirectionRight:
+                        endRect = self.rightRect;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+        } else if (minX < 0) {
+            if (!shouldQuickSwipe) {
+                CGFloat mid = (320 + 320 - self.rightOffSet) / 2;
+                if (maxX < mid) {
+                    endRect = self.leftRect;
+                } else {
+                    endRect = self.centerRect;
+                }
+            } else {
+                switch (self.direction) {
+                    case TPRevealSwipeDirectionLeft:
+                        endRect = self.leftRect;
+                        break;
+                    case TPRevealSwipeDirectionRight:
+                        endRect = self.centerRect;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+        }
+        
+        // animation time
+        CGFloat timeInterval =fabsf(320.0 / self.velocity.x);
+        timeInterval = (timeInterval < 0.2)?timeInterval:0.2;
+        
+        // start animation
+        if(!CGRectEqualToRect(endRect, CGRectZero))
+        {
+            [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+            [UIView animateWithDuration:timeInterval animations:^{
+                self.rootViewController.view.frame = endRect;
+            } completion:^(BOOL Finished){
+                self.rootViewController.view.frame = endRect;
+                self.baseRect = endRect;
+            }];
+        }
+    }
+    
 }
 - (void)gestureRecognizerDidTap:(UIPanGestureRecognizer*)panGesture
 {
-    [self showRootViewController];
+    [self showRootViewControllerAnimated:YES];
 }
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch
 {
-    CGFloat x = self.rootViewController.view.frame.origin.x;
-    
-    BOOL shouldQuickSwipe = NO;
-    if(self.velocity.x > 200.0 || self.velocity.x < -200.0) // 支持快速滑动的手势
-    {
-        shouldQuickSwipe = YES;
+    if (touch.view.tag == 1000) {
+        return NO;
     }
-    
-    // 计算动画时间
-    CGFloat timeInterval =fabsf(320.0 / self.velocity.x);
-    timeInterval = (timeInterval < 0.2)?timeInterval:0.2;
-    
-    // 动画后的位置
-    CGRect endRect = CGRectZero;
-    
-    switch (self.direction) {
-        case TPRevealSwipeDirectionLeft:
-        {
-            CGFloat midX = self.baseRect.origin.x - self.offSet / 2;
-            if(midX > self.minX)
-            {
-                if(x > midX && !shouldQuickSwipe)
-                    endRect = self.baseRect;
-                else
-                    endRect = CGRectMake(self.baseRect.origin.x - self.offSet, self.rootViewController.view.frame.origin.y, self.rootViewController.view.frame.size.width, self.rootViewController.view.frame.size.height);
-            }
-        }
-            break;
-        case TPRevealSwipeDirectionRight:
-        {
-            CGFloat midX = self.baseRect.origin.x + self.offSet / 2;
-            if(midX < self.maxX)
-            {
-                if(x < midX && !shouldQuickSwipe)
-                    endRect = self.baseRect;
-                else
-                    endRect = CGRectMake(self.baseRect.origin.x + self.offSet, self.rootViewController.view.frame.origin.y, self.rootViewController.view.frame.size.width, self.rootViewController.view.frame.size.height);
-            }
-        }
-            break;
-        default:
-            
-            break;
-    }
-    
-    // start animation
-    if(!CGRectEqualToRect(endRect, CGRectZero))
-    {
-        [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-        [UIView animateWithDuration:timeInterval animations:^{
-            self.rootViewController.view.frame = endRect;
-        } completion:^(BOOL Finished){
-            self.rootViewController.view.frame = endRect;
-            self.baseRect = endRect;
-        }];
-    }
-
+    return YES;
 }
-
 #pragma mark animation
--(void)showRootViewController
+-(void)showRootViewControllerAnimated:(BOOL)animate
 {
     if(!self.rootViewController)
         return ;
     
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-    [UIView animateWithDuration:0.19f animations:^{
-        self.rootViewController.view.frame = self.centerRect;
-    } completion:^(BOOL Finished){
+    if(animate)
+    {
+        [UIView beginAnimations:@"ShowRootView" context:nil];
+        [UIView setAnimationDuration:0.3];
+        [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
         self.rootViewController.view.frame = self.centerRect;
         self.baseRect = self.centerRect;
-    }];
+        [UIView commitAnimations];
+    }
+    else
+    {
+        self.rootViewController.view.frame = self.centerRect;
+        self.baseRect = self.centerRect;
+    }
 }
 
--(void)showLeftViewController
+-(void)showLeftViewControllerAnimated:(BOOL)animate
 {
     if(!self.leftViewController)
         return ;
     
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-    [UIView animateWithDuration:0.19f animations:^{
-        self.rootViewController.view.frame = self.rightRect;
-    } completion:^(BOOL Finished){
+    if(self.upperController != TPRevealSwipeDirectionLeft) {
+        [self.view exchangeSubviewAtIndex:0 withSubviewAtIndex:1];
+        self.upperController = TPRevealSwipeDirectionLeft;
+    }
+    
+    if(animate)
+    {
+        [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+        [UIView animateWithDuration:0.3f animations:^{
+            self.rootViewController.view.frame = self.rightRect;
+        } completion:^(BOOL Finished){
+            self.rootViewController.view.frame = self.rightRect;
+            self.baseRect = self.rightRect;
+        }];
+    }
+    else
+    {
         self.rootViewController.view.frame = self.rightRect;
         self.baseRect = self.rightRect;
-    }];
+    }
 }
 
--(void)showRightViewController
+-(void)showRightViewControllerAnimated:(BOOL)animate
 {
     if(!self.rightViewController)
         return ;
     
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-    [UIView animateWithDuration:0.19f animations:^{
-        self.rootViewController.view.frame = self.leftRect;
-    } completion:^(BOOL Finished){
+    if(self.upperController != TPRevealSwipeDirectionRight) {
+        [self.view exchangeSubviewAtIndex:0 withSubviewAtIndex:1];
+        self.upperController = TPRevealSwipeDirectionRight;
+    }
+    
+    if(animate)
+    {
+        [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+        [UIView animateWithDuration:0.3f animations:^{
+            self.rootViewController.view.frame = self.leftRect;
+        } completion:^(BOOL Finished){
+            self.rootViewController.view.frame = self.leftRect;
+            self.baseRect = self.leftRect;
+        }];
+    }
+    else
+    {
         self.rootViewController.view.frame = self.leftRect;
         self.baseRect = self.leftRect;
-    }];
+    }
 }
 
 // 在Left和Right都有的情况下隐藏左边。必须在视图加载后才可调用,因为ViewDidLoad会先Setup一次数据。
--(void)setLeftViewControllerEnable:(BOOL)enable  
+-(void)setLeftViewControllerEnable:(BOOL)enable
 {
     if(!(self.leftViewController && self.rightViewController))
         return ;
     
     if(!enable)  // 通过改变边界值来限制滑动
     {
-        self.minX = - self.offSet;
-        self.maxX = 0.0;
+        self.rightBorder = 0.0;
     }
     else
     {
-        self.minX = - self.offSet;
-        self.maxX = self.offSet;
+        self.rightBorder = self.leftOffSet;
     }
 }
 // 在Left和Right都有的情况下隐藏右边。
@@ -199,83 +281,123 @@
     
     if(!enable)  // 通过改变边界值来限制滑动
     {
-        self.minX = 0.0 ;
-        self.maxX = self.offSet;
+        self.leftBorder = 0.0 ;
     }
     else
     {
-        self.minX = - self.offSet;
-        self.maxX = self.offSet;
+        self.leftBorder = - self.rightOffSet;
     }
+}
+-(void)changeRootViewController:(UIViewController *) rootController
+{
+    [self.rootViewController.view removeFromSuperview];
+    CGRect oldRect = self.rootViewController.view.frame;
+    self.rootViewController = rootController;
+    
+    rootController.view.frame = oldRect;  // Frame
+    if(rootController.view.layer.shadowOpacity == 0.0)    // Shadow
+        [self addShadowToRootView];
+    if([self.view.gestureRecognizers count] == 0)       // pan & tap
+        [self addGestureToRootView];
+    
+    [self.view addSubview:self.rootViewController.view];
+    rootController.view.frame = oldRect;
+    [self showRootViewControllerAnimated:YES];
+}
+
+-(BOOL)isCentered {
+    if (CGRectEqualToRect(self.baseRect, self.centerRect)) {
+        return YES;
+    }
+    return NO;
 }
 
 #pragma mark init
 -(void)setupData
 {
-    self.offSet = 250.0;    // 默认230.0
-    // 原来的view要考虑电池栏,故y为20,现在调整一下
-    self.rootViewController.view.frame = CGRectMake(0, 0, 320.0, self.view.bounds.size.height + 20);
-    self.leftViewController.view.frame = CGRectMake(0, 0, 320.0, self.view.bounds.size.height + 20);
-    self.rightViewController.view.frame = CGRectMake(0, 0, 320.0, self.view.bounds.size.height + 20);
+    self.leftOffSet = 150.0;
+    self.rightOffSet = 230.0;
+    
+    self.rootViewController.view.frame = CGRectMake(0, 0, 320.0, self.view.bounds.size.height + 0);
+    self.leftViewController.view.frame = CGRectMake(0, 0, 320.0, self.view.bounds.size.height + 0);
+    self.rightViewController.view.frame = CGRectMake(0, 0, 320.0, self.view.bounds.size.height + 0);
     self.baseRect = self.rootViewController.view.frame;
-    self.leftRect = CGRectMake(- self.offSet, self.baseRect.origin.y, self.baseRect.size.width, self.baseRect.size.height);
+    self.leftRect = CGRectMake(- self.rightOffSet, self.baseRect.origin.y, self.baseRect.size.width, self.baseRect.size.height);
     self.centerRect = CGRectMake(0.0, self.baseRect.origin.y, self.baseRect.size.width, self.baseRect.size.height);
-    self.rightRect = CGRectMake(self.offSet, self.baseRect.origin.y, self.baseRect.size.width, self.baseRect.size.height);
+    self.rightRect = CGRectMake(self.leftOffSet, self.baseRect.origin.y, self.baseRect.size.width, self.baseRect.size.height);
     self.canSwipe = YES;
     
     if(self.leftViewController && self.rightViewController)
     {
-        self.minX = - self.offSet;
-        self.maxX = self.offSet;
+        self.leftBorder = - self.rightOffSet;
+        self.rightBorder = self.leftOffSet;
     }
     else if(self.leftViewController && !self.rightViewController)
     {
-        self.minX = 0.0;
-        self.maxX = self.offSet;
+        self.leftBorder = 0.0;
+        self.rightBorder = self.leftOffSet;
     }
     else if(!self.leftViewController && self.rightViewController)
     {
-        self.minX = - self.offSet;
-        self.maxX = 0.0;
+        self.leftBorder = - self.rightOffSet;
+        self.leftBorder = 0.0;
     }
     else
     {
-        self.minX = 0.0;
-        self.maxX = 0.0;
+        self.leftBorder = 0.0;
+        self.rightBorder = 0.0;
     }
     
-    self.pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(gestureRecognizerDidPan:)];
-    self.pan.cancelsTouchesInView = NO;
-    [self.view addGestureRecognizer:self.pan];  // 当前的View处理触摸事件
     
-    self.tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(gestureRecognizerDidTap:)];
-    [self.view addGestureRecognizer:self.tap];
 }
+- (void)setLeftOffSet:(CGFloat)leftOffSet
+{
+    _leftOffSet = leftOffSet;
+    self.rightRect = CGRectMake(leftOffSet, self.baseRect.origin.y, self.baseRect.size.width, self.baseRect.size.height);
+}
+
 -(void)setupViewController
 {
-    if(self.rightViewController)
+    if(self.rightViewController) {
+        [self addChildViewController:self.rightViewController];
         [self.view addSubview:self.rightViewController.view];
-    if(self.leftViewController)
+    }
+    if(self.leftViewController) {
+        [self addChildViewController:self.leftViewController];
         [self.view addSubview:self.leftViewController.view];
+    }
     if(self.rootViewController)
     {
+        [self addChildViewController:self.rootViewController];
         [self.view addSubview:self.rootViewController.view];
         [self addShadowToRootView];
+        [self addGestureToRootView];
     }
     
-    if(self.leftViewController && self.rightViewController)  // 初始化相对顺序在上面的视图是左边的
+    if(self.leftViewController && self.rightViewController)
         self.upperController = TPRevealSwipeDirectionLeft;
     
+}
+-(void)addGestureToRootView
+{
+    self.pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(gestureRecognizerDidPan:)];
+    self.pan.cancelsTouchesInView = NO;
+    [self.rootViewController.view addGestureRecognizer:self.pan];
+    
+    self.tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(gestureRecognizerDidTap:)];
+    self.tap.delegate = self;
+    [self.rootViewController.view addGestureRecognizer:self.tap];
 }
 -(void)addShadowToRootView
 {
     self.rootViewController.view.layer.shadowPath = [UIBezierPath bezierPathWithRect:self.rootViewController.view.bounds].CGPath;
     self.rootViewController.view.layer.shadowOffset = CGSizeMake(0, 3);
-    self.rootViewController.view.layer.shadowRadius = 10.0;
+    self.rootViewController.view.layer.shadowRadius = 7.0;
     self.rootViewController.view.layer.shadowColor = [UIColor blackColor].CGColor;
     self.rootViewController.view.layer.shadowOpacity = 0.8;
     self.rootViewController.view.layer.masksToBounds = NO;
 }
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
